c11fde7b9e40319ca4a03ecb8854aea38913af78
RazibBP <mdrazib.dev@gmail.com>
2025-05-21 12:07:00 +0600

    javascript string methods code.

---
+++ string_methods.js
@@ -0,0 +1,11 @@
+let x = "Hello world. Today is wendnesday. so today my univerciti off day."
+
+console.log(x.length)
+console.log(x.slice(12,65))
+console.log(x.substring(6))
+console.log(x.substr(6,12))
+
+let y = "I love coding and trading. Today finish my teading. Today iT was 11 Tred and 6 win ouT of 11 Tead. now i am learn and Try To code javascript and html."
+z = y.replace("trading","watching movie");
+console.log(z)
+
