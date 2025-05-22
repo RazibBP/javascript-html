195afdb1bded43e5d28cf15cf3a76222288e34d5
RazibBP <mdrazib.dev@gmail.com>
2025-05-22 23:34:00 +0600

    javascript Arry

---
+++ arry.js
@@ -0,0 +1,18 @@
+const x =["mango","apple","banana","painaple","kiwi"];
+
+console.log(x)
+console.log(x.length)// Arry reaturn of the arry size and number 
+console.log(typeof x)// typeof gives information of method type
+
+x.push("orange") // Arry push / add arry last number
+let y =  x.pop('') 
+
+const a = ['rakib','nirob','jeet','rajib','shakhor'];
+
+z = x.concat(a) //two arry add 
+console.log(z)
+
+a.splice(3,0,'sumiya') //add arry 
+a.splice(5,0,'Eva')
+
+
