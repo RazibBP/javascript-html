de984e472f71476267d1443f8ed0cf56ef9386fd
RazibBP <mdrazib.dev@gmail.com>
2025-05-24 17:57:00 +0600

    javascript problem switch code

---
+++ switch.js
@@ -0,0 +1,28 @@
+// const days = new Date().getDay()
+// console.log(days)
+const days = new Date().getDay();
+switch(days){
+
+    case 0:
+        day = "sunday"
+        break;
+    case 1:
+        day = "Monday"
+        break;
+    case 2:
+        day = "Tuesday"
+        break;
+    case 3:
+        day = 'Wedneday'
+        break;
+    case 4:
+        day = "Thureday"
+        break;
+    case 5:
+        day = "Friday"
+        break;
+    case 6:
+        day = "Saturday"
+}
+let = day;
+console.log()
