3726f11b8a90f981abc774912553a2a655ba21fd
RazibBP <mdrazib.dev@gmail.com>
2025-05-24 17:16:03 +0600

    javascript problem if else

---
+++ problem_solve_if_else.js
@@ -0,0 +1,10 @@
+const age = 18;
+let result;
+
+if(age >= 18 && age <30){
+    result = "You are adult!"
+} else if (age > 30){
+    result = "You are an old!"
+} else{
+    result = "You are young!"
+}
