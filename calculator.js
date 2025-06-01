8b72cda7467d8d96a37d76cf3cf3cd1bc7749e8d
RazibBP <mdrazib.dev@gmail.com>
2025-06-01 19:39:40 +0600

    javascript problem calculaor

--- calculator.js
+++ calculator.js
@@ -1,12 +1,63 @@
 let display = document.getElementById('display');
 
-function addValue(){
-
+function addV(){
+    display.value += '7';
 }
 
-function allClear(){
-
+function add(){
+    display.value = '';
 }
-function oneClear(){
-
+function onedel(){
+    display.value = display.value.toString().slice(0,-1)
+}
+function eva(){
+    display.value = eval(display.value)
+}
+function adm(){
+    display.value  += '+'
+}
+function par(){
+    display.value += '%'
+}
+function divison(){
+    display.value += '/'
+}
+function mul(){
+    display.value +='*'
+}
+function sub(){
+    display.value +='-'
+}
+function dzero(){
+    display.value += '00'
+}
+function zero(){
+    display.value += '0'
+}
+function one(){
+    display.value += '1'
+}
+function two(){
+    display.value += '2'
+}
+function three(){
+    display.value += '3'
+}
+function four(){
+    display.value += '4'
+}
+function five(){
+    display.value +='5'
+}
+function six(){
+    display.value += '6'
+}
+function eight(){
+    display.value += '8'
+}
+function nine(){
+    display.value +='9'
+}
+function dot(){
+    display.value += '.'
 }
--- calculator_problem.js
+++ calculator_problem.js
@@ -1,16 +1,44 @@
 const display = document.getElementById('display');
-const values = document.getElementsByClassName('addValue')
+// const values = document.getElementsByClassName('addValue')
 
-let string ='' ;
-let arr  =  Array.from(values);
+// let string ='' ;
+// let arr  =  Array.from(values);
 
-console.log(values);
+// console.log(values);
 
-arr.forEach(function(values){
-    if (arr.values == '='){
-        string = eval(values);
-    }
-    else {
-        string +=values;
-    }
-})
+// arr.forEach(function(values){
+//     if (arr.values == '='){
+//         string = eval(values);
+//     }
+//     else {
+//         string +=values;
+//     }
+// })
+let sav = document.getElementById('sava') 
+function sava() {display.value += '7'};
+
+let eights = document.getElementById('eig') 
+function eight() {display.value += '8'};
+
+let sixx = document.getElementById('sixxx') 
+function six() {display.value += '6'};
+
+let ni = document.getElementById('ninn') 
+function nine() {display.value += '9'};
+
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
+// let sav = document.getElementById('sava') 
+// function sava() {display.value += '7'};
